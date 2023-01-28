06d9b14568123ae91c3d5d9481ac338e600c9f60
mohid <mohid10587@gmail.com>
2023-01-29 00:57:41 +0600

    ok

--- index.js
+++ index.js
@@ -15,22 +15,6 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7o5d5sd.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-// verify token
-
-function verifyJWT(req, res, next) {
-    const authHeader = req.headers.authorization;
-    if (!authHeader) {
-        return res.status(401).send({ message: "Unauthorized access" })
-    }
-    const token = authHeader.split(' ')[1];
-    jwt.verify(token, process.env.ACCESS_TOKEN_VAR, function (err, decoded) {
-        if (err) {
-            return res.status(403).send({ massage: 'forbidden' })
-        }
-        req.decoded = decoded;
-        next();
-    });
-}
 
 
 
@@ -41,17 +25,7 @@ async function run() {
         console.log('connected')
         const correctionsCollection = client.db('Knowledge').collection('correction')
 
-        const verifyAdmin = async (req, res, next) => {
-            const requester = req.decoded.email
-            const requesterAccount = await usersCollection.findOne({ email: requester })
-
-            if (requesterAccount.roll === 'admin') {
-                next()
-            } else {
-                res.status(403).send({ massage: 'Forbidden' })
-            }
-        }
-
+     
         app.post('/correction', async (req, res) => {
             const correction = req.body;
             const result = await correctionsCollection.insertOne(correction);
