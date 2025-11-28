b9f40e94bc17eb59a35aa4e2afaf331b3f90ac9f
mohid <mohid10587@gmail.com>
2025-11-28 12:30:40 +0600

    Re-add gitignore and remove large tracked files

--- index.js
+++ index.js
@@ -1,71 +1,67 @@
-const express = require('express')
-const jwt = require('jsonwebtoken')
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const cors = require('cors');
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const cors = require("cors");
 
+require("dotenv").config();
 
-require('dotenv').config()
-
-const app = express()
-const port = process.env.PORT || 5001
-
-app.use(cors())
-app.use(express.json())
+const app = express();
+const port = process.env.PORT || 5001;
+// Allow only specific URLs
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://dawah.netlify.app",
+  "https://dawah2.vercel.app",
+];
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
+app.use(express.json());
 
 const uri = `mongodb://mohid:m0EK5JN4EzQnQwpP@ac-c1hfgxl-shard-00-00.7o5d5sd.mongodb.net:27017,ac-c1hfgxl-shard-00-01.7o5d5sd.mongodb.net:27017,ac-c1hfgxl-shard-00-02.7o5d5sd.mongodb.net:27017/?ssl=true&replicaSet=atlas-446w00-shard-0&authSource=admin&retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
-
-
-
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 async function run() {
-    try {
-        await client.connect()
-        console.log('connected')
-        const correctionsCollection = client.db('Knowledge').collection('correction')
+  try {
+    await client.connect();
+    console.log("connected");
+    const correctionsCollection = client
+      .db("Knowledge")
+      .collection("correction");
 
-     
-        app.post('/correction', async (req, res) => {
-            const correction = req.body;
-            const result = await correctionsCollection.insertOne(correction);
-            res.send(result);
-      
-          })
-          app.get('/corrections', async (req, res) => {
-            const correction = await correctionsCollection.find().toArray()
-            res.send(correction)
-          })
+    app.post("/correction", async (req, res) => {
+      const correction = req.body;
+      const result = await correctionsCollection.insertOne(correction);
+      res.send(result);
+    });
+    app.get("/corrections", async (req, res) => {
+      const correction = await correctionsCollection.find().toArray();
+      res.send(correction);
+    });
 
-          app.delete('/deleteOrder/:id', async (req, res) => {
-            const id = req.params.id;
-            const filter = { _id: ObjectId(id) };
-            const result = await correctionsCollection.deleteOne(filter);
-            res.send(result);
-        })
-    } finally {
-
-    }
-
+    app.delete("/deleteOrder/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const result = await correctionsCollection.deleteOne(filter);
+      res.send(result);
+    });
+  } finally {
+  }
 }
 
+run().catch(console.dir);
 
-run().catch(console.dir)
-
-
-
-app.get('/', async (req, res) => {
-    res.send('This is first deployment in heroku')
-})
+app.get("/", async (req, res) => {
+  res.send("This is first deployment in heroku");
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
-
-
-
-
-
-
-
-
+  console.log(`Example app listening on port ${port}`);
+});
